236dd064a2c0ed5724851e7329f3ad99b1b50d7f
jdmcclur@us.ibm.com <jdmcclur@us.ibm.com>
2021-02-05 15:57:09 -0600

    updates, give up on kz

--- src/main/java/com/acmeair/restclient/CustomerClient.java
+++ src/main/java/com/acmeair/restclient/CustomerClient.java
@@ -22,8 +22,8 @@ import java.io.IOException;
 import java.time.temporal.ChronoUnit;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.GET;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -33,22 +33,21 @@ import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import javax.ws.rs.core.MediaType;
-
 @Path("/internal")
 @RegisterRestClient(configKey="customerClient")
 @ApplicationScoped
 public interface CustomerClient {
 
-  @GET
-  @Path("/validateid/{login}/{password}")
-  @Produces(MediaType.APPLICATION_JSON)
+  @POST
+  @Path("/validateid")
+  @Consumes({ "application/x-www-form-urlencoded" })
+  @Produces("application/json")
   @Timeout(500) 
   @CircuitBreaker(requestVolumeThreshold=4,failureRatio=0.5,successThreshold=10,delay=1,delayUnit=ChronoUnit.SECONDS)
   @Retry(maxRetries = 3, delayUnit = ChronoUnit.SECONDS, delay = 5, durationUnit = ChronoUnit.SECONDS, 
     maxDuration = 30,retryOn = Exception.class, abortOn = IOException.class)
   @Fallback(LoginFallbackHandler.class)
   public LoginResponse validateCustomer(
-      @PathParam("login") String login, 
-      @PathParam("password") String password);
+      @FormParam("login") String login, 
+      @FormParam("password") String password);
 }
